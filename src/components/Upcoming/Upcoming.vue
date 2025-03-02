d7b53b988f762c3f819478f09bcdfaa6c6647057
Danil <danilser2005@gmail.com>
2025-03-02 18:19:51 +0500

    fixed navbar

--- src/components/Upcoming/Upcoming.vue
+++ src/components/Upcoming/Upcoming.vue
@@ -1,11 +1,50 @@
 <template>
+    <Transition name="upcoming" mode="out-in">
+      <section class="main__upcoming" v-if="upcomings">
+        <UpcomingItem 
+        v-for="(movie, idx) in upcomings" 
+        :key="movie.id"
+        :movie="movie"
+        :idx="idx"
+        :slideView="slideView"
+        :next="upcomings[idx + 1 == upcomings.length ? 0 : idx + 1]" 
+        @slideNext="slideNext" />
+      </section>
+      <div class="loading" v-else>
+        <div class="loading__spiner"></div>
+      </div>
+    </Transition>
+  </template>
   
-</template>
-
-<script setup>
-
-</script>
-
-<style>
-
-</style>
+  <script setup>
+  import UpcomingItem from "@/components/Upcoming/UpcomingItem.vue";
+  import { useUpcomingStore } from '@/store/modules/upcomingStore.js'
+  import { ref, onMounted } from 'vue'
+  
+  const upcomings = ref(null)
+  const slideView = ref(0)
+  const timeout = ref(null)
+  
+  const slide = () => {
+    if (upcomings.value.length - 1 == slideView.value) {
+      slideView.value = 0
+    } else {
+      slideView.value++
+    }
+    timeout.value = setTimeout(slide, 10000);
+  }
+  
+  const slideNext = () => {
+    clearTimeout(timeout.value)
+    slide()
+  }
+  
+  let upcomingStore = useUpcomingStore()
+  onMounted(async () => {
+    await upcomingStore.getUpcoming()
+    upcomings.value = upcomingStore.upcoming
+    timeout.value = setTimeout(slide, 10000);
+  })
+  </script>
+  
+  <style></style>
